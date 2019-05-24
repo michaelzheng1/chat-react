5a1ca78a8b1212a7394660cdb6437a6856f8f5fd
michaelzheng1 <michaelzheng0128@gmail.com>
2019-05-24 12:02:29 -0400

    adding message list

--- chat/src/components/MessageList.js
+++ chat/src/components/MessageList.js
@@ -1,32 +1,78 @@
-import React from 'react'
+// import React from 'react'
 
-const DUMMY_DATA = [
-    {
-        senderId: 'perborgen',
-        text: 'Hey'
-    },
-    {
-        senderId: 'janedoe',
-        text: 'Great!'
-    },
-    {
-        senderId: 'perborgen',
-        text: 'Good'
-    }
-]
+// const DUMMY_DATA = [
+//     {
+//         senderId: 'perborgen',
+//         text: 'Hey'
+//     },
+//     {
+//         senderId: 'janedoe',
+//         text: 'Great!'
+//     },
+//     {
+//         senderId: 'perborgen',
+//         text: 'Good'
+//     }
+// ]
+// class MessageList extends React.Component {
+//     render() {
+//         return (
+//             <div className="message-list">
+//                 {DUMMY_DATA.map((message, index) => {
+//                     return (
+//                         <div>
+//                             <div>{message.senderId}</div>
+//                             <div>{message.tex}</div>
+
+//                         </div>
+//                     )
+//                 })}
+//             </div>
+//         )
+//     }
+// }
+
+// export default MessageList
+
+import React, {Component} from 'react'
+
 class MessageList extends React.Component {
     render() {
+        const styles = {
+            container: {
+                overflowY: 'scroll',
+                flex:1,
+            },
+            ul: {
+                listStyle: 'none',
+            }, 
+            li: {
+                marginTop: 13,
+                marginBottom: 13,
+            },
+            senderUsername: {
+                fontWeigth: 'bold',
+            },
+            message: {fontSize: 15},
+        }
         return (
-            <div className="message-list">
-                {DUMMY_DATA.map((message, index) => {
-                    return (
-                        <div>
-                            <div>{message.senderId}</div>
-                            <div>{message.tex}</div>
-
-                        </div>
-                    )
-                })}
+            <div
+                style= {{
+                    ...this.props.style,
+                    ...styles.container,
+                }}
+                >
+                <ul style={styles.ul}>
+                    {this.props.messages.map((message,index) => (
+                        <li key={index} style={styles.li}>
+                            <div>
+                                <span style={styles.senderUsername}>{message.senderId}</span>
+                                {' '}
+                            </div>
+                            <p style={styles.message}>{message.text}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
